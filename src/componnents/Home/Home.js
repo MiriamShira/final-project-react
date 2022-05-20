183536e9b5952c4095b999f6c17a552e6f7f1bcb
Shira Eldad <sandm2360@gmail.com>
2022-05-20 04:31:56 +0300

    add navigation to home

--- src/componnents/Home/Home.js
+++ src/componnents/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ImageUpload from './uploadingimage';
 
-
+import Navigate from '../navigate'
 export default function Home() {
   return (
     <div>
+      <Navigate/>
       < ImageUpload/>
     </div>
   );
